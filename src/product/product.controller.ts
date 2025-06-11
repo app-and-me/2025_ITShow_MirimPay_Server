be4577f9c383b363f2f7c4d3ae54b1df62f65749
jaehokang1007 <jaehokang1007@icloud.com>
2025-06-11 19:07:16 +0900

    feat: 문의, 제품 JWT 가드 추가

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,16 +6,19 @@ import {
   Delete,
   Param,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() dto: CreateProductDto) {
     return this.productService.create(dto);
   }
@@ -31,11 +34,13 @@ export class ProductController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
     return this.productService.update(id, dto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.productService.remove(id);
   }
